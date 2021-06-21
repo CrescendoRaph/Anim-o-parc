4cbda3f7f8fcadbc175930a6e98fe54b4e280bb4
CrescendoRaph <raphael.r21000@gmail.com>
2021-06-21 17:38:25 +0200

    test formulaire

---
+++ assets/php/contact.php
@@ -0,0 +1,42 @@
+<?php 
+    require_once(__DIR__ . '/vendor/autoload.php');
+    use \Mailjet\Resources;
+    define('API_USER', '0a6ea21283f1001cd82bc9463004026f');
+    define('API_LOGIN', 'c9ff88f7b9060cefe34fbf5c5cde177a');
+    $mj = new \Mailjet\Client(API_USER, API_LOGIN,true,['version' => 'v3.1']);
+
+    if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])){
+        $surname = htmlspecialchars($_POST['surname']);
+        $firstname = htmlspecialchars($_POST['firstname']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $body = [
+            'Messages' => [
+                [
+                  'From' => [
+                    'Email' => "rondoudou21@gmail.com",
+                    'Name' => "Raphael"
+                  ],
+                  'To' => [
+                    [
+                      'Email' => "rondoudou21@gmail.com",
+                      'Name' => "Raphael"
+                    ]
+                  ],
+                  'Subject' => "Greetings from Mailjet.",
+                  'TextPart' => "My first Mailjet email",
+                  'CustomID' => "AppGettingStartedTest"
+                ]
+              ]
+            ];
+            $response = $mj->post(Resources::$Email, ['body' => $body]);
+            $response->success();
+            echo "Email envoyé avec succès !";
+        }
+        else{
+            echo "Email non valide";
+        }
+
+    }
---
+++ assets/php/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact us</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+</head>
+
+<body>
+
+	<a href="index.html">
+		<img src="images/logo_animo.png.png" height="100px" width="100px" id="logo" alt="Logo"/>
+	</a>
+
+	<div class="container">
+		<form action="/assets/php/contact.php" method="POST">
+			<div class="contact-box">
+				<div class="left"></div>
+				<div class="right">
+					<h2>Contact Us</h2>
+					<input type="text" name="surname" class="field" placeholder="Votre Nom">
+                    <input type="text" name="firstname" class="field" placeholder="Prenom">
+					<input type="email" name="email" class="field" placeholder="Votre Email">
+					<textarea placeholder="Votre Message" name="message" class="field"></textarea>
+					<button type="submit" class="btn">Send</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
